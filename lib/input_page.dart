e33f217c37ad356cf8b229f8dcc5e177a1fcf06b
Sherry-jsp <pjhoannasherry28@gmail.com>
2023-06-29 11:21:35 +0800

    refactor(separation):custom widgets in different dart files

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 50.0;
 const boxColor = Color(0xFF1D1E33);
@@ -26,25 +28,37 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(colour:boxColor),
+                    child: ReusableCard(
+                      colour: boxColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ReusableCard(colour:boxColor),
+                    child: ReusableCard(
+                      colour: boxColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child:ReusableCard(colour:boxColor),
+              child: ReusableCard(colour: boxColor),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(colour:boxColor),
+                    child: ReusableCard(colour: boxColor),
                   ),
                   Expanded(
-                    child: ReusableCard(colour:boxColor),
+                    child: ReusableCard(colour: boxColor),
                   ),
                 ],
               ),
@@ -62,21 +76,3 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, this.cardChild}); //constructor
-
-  final Color colour;
-  final Widget? cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: cardChild,
-    );
-  }
-}
